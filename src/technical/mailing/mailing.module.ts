50013e46de9e4624aa7e7d580f937df79105d19b
Bartosz Śnieżek <bartosz.sniezek@outlook.com>
2026-02-22 10:39:25 +0100

    Add mailing module

---
+++ src/technical/mailing/mailing.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { MAIL_SERVICE as MAILING_SERVICE } from './constants';
+import { AppConfig } from '@technical/app-config/app-config';
+import { TransportMode } from './transport-mode.enum';
+import { MailService } from './interfaces/mail-service';
+import { ConsoleMailService } from './console-mail.service';
+import { NodemailerMailService } from './nodemailer-mail.service';
+import { AppConfigModule } from '@technical/app-config/app-config.module';
+
+@Module({
+  imports: [AppConfigModule],
+  providers: [
+    {
+      provide: MAILING_SERVICE,
+      useFactory: (appConfig: AppConfig): MailService => {
+        switch (appConfig.values.MAILER_TRANSPORT_MODE) {
+          case TransportMode.CONSOLE:
+            return new ConsoleMailService();
+          case TransportMode.SMTP:
+            return new NodemailerMailService(appConfig);
+          default:
+            throw new Error(
+              `Unsupported transport: ${appConfig.values.MAILER_TRANSPORT_MODE}`,
+            );
+        }
+      },
+      inject: [AppConfig],
+    },
+  ],
+  exports: [MAILING_SERVICE],
+})
+export class MailingModule {}
---
+++ src/technical/mailing/transport-mode.enum.ts
@@ -0,0 +1,4 @@
+export enum TransportMode {
+  CONSOLE = 'console',
+  SMTP = 'smtp',
+}
