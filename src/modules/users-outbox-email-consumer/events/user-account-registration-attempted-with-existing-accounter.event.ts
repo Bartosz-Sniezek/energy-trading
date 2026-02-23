6c25011245a91a0b929e4a1ef7bf5ce0e5687ad6
Bartosz Śnieżek <bartosz.sniezek@outlook.com>
2026-02-23 07:52:28 +0100

    Add UserAccountRegistrationAttemptedWithExistingAccountEvent

---
+++ src/modules/users-outbox-email-consumer/events/user-account-registration-attempted-with-existing-accounter.event.ts
@@ -0,0 +1,61 @@
+import z from 'zod';
+import { DebeziumOutboxMessage } from '../debezium-connector-message.parser';
+import { InvalidPayloadDataError } from '@common/errors/invalid-payload-data.error';
+import { Email } from '@domain/users/value-objects/email';
+import { UserEvents } from '@domain/users/events.enum';
+import { InvalidEventTypeError } from '@common/errors/invalid-event-type.error';
+
+export const userAccountRegistrationAttemptedWithExistingAccountEventSchema =
+  z.object({
+    id: z.uuidv7(),
+    userId: z.uuidv7(),
+    email: z.email().transform((data) => Email.create(data)),
+    firstName: z.string(),
+    lastName: z.string(),
+    timestamp: z.string().transform((data) => parseInt(data)),
+  });
+
+export type TUserAccountRegistrationAttemptedWithExistingAccountEvent = z.infer<
+  typeof userAccountRegistrationAttemptedWithExistingAccountEventSchema
+>;
+
+export class UserAccountRegistrationAttemptedWithExistingAccountEvent implements TUserAccountRegistrationAttemptedWithExistingAccountEvent {
+  readonly id: string;
+  readonly userId: string;
+  readonly email: Email;
+  readonly firstName: string;
+  readonly lastName: string;
+  readonly timestamp: number;
+
+  private constructor(
+    options: TUserAccountRegistrationAttemptedWithExistingAccountEvent,
+  ) {
+    Object.assign(this, options);
+  }
+
+  static parse(
+    event: DebeziumOutboxMessage,
+  ): UserAccountRegistrationAttemptedWithExistingAccountEvent {
+    if (
+      event.eventType !==
+      UserEvents.USER_ACCOUNT_REGISTRATION_ATTEMPTED_WITH_EXISTING_ACCOUNT
+    ) {
+      throw new InvalidEventTypeError(event.eventType);
+    }
+
+    const payload = event.payload;
+    const { data, error } =
+      userAccountRegistrationAttemptedWithExistingAccountEventSchema.safeParse({
+        id: event.id,
+        userId: event.aggregateId,
+        email: payload?.['email'],
+        firstName: payload?.['firstName'],
+        lastName: payload?.['lastName'],
+        timestamp: event.timestamp,
+      });
+
+    if (error) throw new InvalidPayloadDataError(error.flatten().fieldErrors);
+
+    return new UserAccountRegistrationAttemptedWithExistingAccountEvent(data);
+  }
+}
