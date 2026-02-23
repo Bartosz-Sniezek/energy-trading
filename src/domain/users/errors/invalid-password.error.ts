12fa4b81bbab44c12b1993fad4c84d116a56acc9
Bartosz Śnieżek <bartosz.sniezek@outlook.com>
2026-02-23 18:37:56 +0100

    Add InvalidPasswordError

---
+++ src/domain/users/errors/invalid-password.error.ts
@@ -0,0 +1,7 @@
+import { DomainError } from '@domain/errors/domain.error';
+
+export class InvalidPasswordError extends DomainError {
+  constructor(errors: string[]) {
+    super(`Invalid password: ${errors.join(', ')}`);
+  }
+}
