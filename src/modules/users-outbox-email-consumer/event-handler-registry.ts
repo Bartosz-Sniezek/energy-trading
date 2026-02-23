c51d7cc8953b0fddad2597d56dc1a5d5773377ca
Bartosz Śnieżek <bartosz.sniezek@outlook.com>
2026-02-23 15:32:33 +0100

    Add UserAccountActivatedHandler to EventHandlerRegistry

--- src/modules/users-outbox-email-consumer/event-handler-registry.ts
+++ src/modules/users-outbox-email-consumer/event-handler-registry.ts
@@ -4,22 +4,24 @@ import { UserAccountCreatedHandler } from './handlers/user-account-created.handl
 import { UserOutboxMessageHandler } from './interfaces/user-outbox-message-handler';
 import { UserEvents } from '@domain/users/events.enum';
 import { UnsupportedEventTypeError } from './errors/unsupported-event-type.error';
+import { UserAccountActivatedHandler } from './handlers/user-account-activated.handler';
 
 @Injectable()
 export class EventHandlerRegistry {
-  private readonly handlers = new Map<string, UserOutboxMessageHandler>();
+  private readonly handlers: Map<string, UserOutboxMessageHandler>;
   private readonly logger: Logger;
 
   constructor(
     userAccountCreatedHandler: UserAccountCreatedHandler,
+    userAccountActivatedHandler: UserAccountActivatedHandler,
     @Optional()
     logger?: Logger,
   ) {
     this.logger = logger ? logger : new Logger(EventHandlerRegistry.name);
-    this.handlers.set(
-      UserEvents.USER_ACCOUNT_REGISTERED,
-      userAccountCreatedHandler,
-    );
+    this.handlers = new Map<string, UserOutboxMessageHandler>([
+      [UserEvents.USER_ACCOUNT_REGISTERED, userAccountCreatedHandler],
+      [UserEvents.USER_ACCOUNT_ACTIVATED, userAccountActivatedHandler],
+    ]);
   }
 
   async handle(event: DebeziumOutboxMessage): Promise<void> {
