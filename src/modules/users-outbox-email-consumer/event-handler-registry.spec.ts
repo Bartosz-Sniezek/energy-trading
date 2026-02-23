f6c8f2899287eaf786239c239de2523748f8755b
Bartosz Śnieżek <bartosz.sniezek@outlook.com>
2026-02-23 15:32:33 +0100

    Add UserAccountActivatedHandler to EventHandlerRegistry

--- src/modules/users-outbox-email-consumer/event-handler-registry.spec.ts
+++ src/modules/users-outbox-email-consumer/event-handler-registry.spec.ts
@@ -5,21 +5,34 @@ import { DebeziumOutboxMessage } from './debezium-connector-message.parser';
 import { UserEvents } from '@domain/users/events.enum';
 import { loggerMock } from 'test/helpers/logger.mock';
 import { UnsupportedEventTypeError } from './errors/unsupported-event-type.error';
+import { UserAccountActivatedHandler } from './handlers/user-account-activated.handler';
 
 describe(EventHandlerRegistry.name, () => {
   const userAccountCreatedHandlerMock = mock<UserAccountCreatedHandler>();
+  const userAccountActivatedHandlerMock = mock<UserAccountActivatedHandler>();
   const registry = new EventHandlerRegistry(
     userAccountCreatedHandlerMock,
+    userAccountActivatedHandlerMock,
     loggerMock,
   );
 
   describe(registry.handle.name, () => {
-    it('should resolve when event is supported', async () => {
+    it('should resolve for USER_ACCOUNT_REGISTERED event', async () => {
       const eventMock = mock<DebeziumOutboxMessage>({
         eventType: UserEvents.USER_ACCOUNT_REGISTERED,
       });
 
       await expect(registry.handle(eventMock)).toResolve();
+      expect(userAccountCreatedHandlerMock.handle).toHaveBeenCalledTimes(1);
+    });
+
+    it('should resolve for USER_ACCOUNT_REGISTERED event', async () => {
+      const eventMock = mock<DebeziumOutboxMessage>({
+        eventType: UserEvents.USER_ACCOUNT_ACTIVATED,
+      });
+
+      await expect(registry.handle(eventMock)).toResolve();
+      expect(userAccountActivatedHandlerMock.handle).toHaveBeenCalledTimes(1);
     });
 
     it('should throw UnsupportedEventTypeError when event is not supported', async () => {
