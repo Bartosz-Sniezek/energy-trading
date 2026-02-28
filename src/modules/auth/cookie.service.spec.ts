d12ab185e6f4bda08694344cde9c278e225cb6dd
Bartosz Śnieżek <bartosz.sniezek@outlook.com>
2026-02-28 20:05:31 +0100

    Add CookieService

---
+++ src/modules/auth/cookie.service.spec.ts
@@ -0,0 +1,165 @@
+import { mock, MockProxy, mockReset } from 'vitest-mock-extended';
+import { CookieService } from './cookie.service';
+import { AppConfig } from '@technical/app-config/app-config';
+import { type Response } from 'express';
+import { randomBytes } from 'crypto';
+import { AccessToken, RefreshToken } from '@domain/auth/types';
+
+describe(CookieService.name, () => {
+  const appConfigMock = mock<AppConfig>({
+    values: {
+      JWT_ACCESS_TOKEN_EXPIRATION_SEC: 60,
+      JWT_REFRESH_TOKEN_EXPIRATION_SEC: 120,
+    },
+  });
+  const accessToken = randomBytes(64).toString('hex') as AccessToken;
+  const refreshToken = randomBytes(64).toString('hex') as RefreshToken;
+  const resMock = mock<Response>();
+  const cookieService = new CookieService(appConfigMock);
+  const options = {
+    res: resMock,
+    accessToken,
+    refreshToken,
+  };
+
+  beforeEach(() => {
+    mockReset(appConfigMock);
+    mockReset(resMock);
+  });
+
+  describe(cookieService.configure.name, () => {
+    describe('NODE_ENV=production', () => {
+      beforeEach(() => {
+        appConfigMock.isProduction.mockReturnValue(true);
+      });
+
+      it('should set access_token cookie', () => {
+        cookieService.configure(options);
+
+        expect(resMock.cookie).toHaveBeenCalledWith(
+          'access_token',
+          accessToken,
+          {
+            httpOnly: true,
+            secure: true,
+            sameSite: 'strict',
+            maxAge: appConfigMock.values.JWT_ACCESS_TOKEN_EXPIRATION_SEC * 1000,
+          },
+        );
+      });
+
+      it('should set refresh_token cookie', () => {
+        cookieService.configure(options);
+
+        expect(resMock.cookie).toHaveBeenCalledWith(
+          'refresh_token',
+          refreshToken,
+          {
+            httpOnly: true,
+            secure: true,
+            sameSite: 'strict',
+            path: '/api/auth/refresh',
+            maxAge:
+              appConfigMock.values.JWT_REFRESH_TOKEN_EXPIRATION_SEC * 1000,
+          },
+        );
+      });
+
+      it('should call response.cookie exactly twice', () => {
+        cookieService.configure(options);
+
+        expect(resMock.cookie).toHaveBeenCalledWith(
+          'access_token',
+          accessToken,
+          {
+            httpOnly: true,
+            secure: true,
+            sameSite: 'strict',
+            maxAge: appConfigMock.values.JWT_ACCESS_TOKEN_EXPIRATION_SEC * 1000,
+          },
+        );
+        expect(resMock.cookie).toHaveBeenCalledWith(
+          'refresh_token',
+          refreshToken,
+          {
+            httpOnly: true,
+            secure: true,
+            sameSite: 'strict',
+            path: '/api/auth/refresh',
+            maxAge:
+              appConfigMock.values.JWT_REFRESH_TOKEN_EXPIRATION_SEC * 1000,
+          },
+        );
+
+        expect(resMock.cookie).toHaveBeenCalledTimes(2);
+      });
+    });
+
+    describe('NODE_ENV != production', () => {
+      beforeEach(() => {
+        appConfigMock.isProduction.mockReturnValue(false);
+      });
+
+      it('should set access_token cookie', () => {
+        cookieService.configure(options);
+
+        expect(resMock.cookie).toHaveBeenCalledWith(
+          'access_token',
+          accessToken,
+          {
+            httpOnly: true,
+            secure: false,
+            sameSite: 'lax',
+            maxAge: appConfigMock.values.JWT_ACCESS_TOKEN_EXPIRATION_SEC * 1000,
+          },
+        );
+      });
+
+      it('should set refresh_token cookie', () => {
+        cookieService.configure(options);
+
+        expect(resMock.cookie).toHaveBeenCalledWith(
+          'refresh_token',
+          refreshToken,
+          {
+            httpOnly: true,
+            secure: false,
+            sameSite: 'lax',
+            path: '/api/auth/refresh',
+            maxAge:
+              appConfigMock.values.JWT_REFRESH_TOKEN_EXPIRATION_SEC * 1000,
+          },
+        );
+      });
+
+      it('should call response.cookie exactly twice', () => {
+        cookieService.configure(options);
+
+        expect(resMock.cookie).toHaveBeenCalledWith(
+          'access_token',
+          accessToken,
+          {
+            httpOnly: true,
+            secure: false,
+            sameSite: 'lax',
+            maxAge: appConfigMock.values.JWT_ACCESS_TOKEN_EXPIRATION_SEC * 1000,
+          },
+        );
+        expect(resMock.cookie).toHaveBeenCalledWith(
+          'refresh_token',
+          refreshToken,
+          {
+            httpOnly: true,
+            secure: false,
+            sameSite: 'lax',
+            path: '/api/auth/refresh',
+            maxAge:
+              appConfigMock.values.JWT_REFRESH_TOKEN_EXPIRATION_SEC * 1000,
+          },
+        );
+
+        expect(resMock.cookie).toHaveBeenCalledTimes(2);
+      });
+    });
+  });
+});
---
+++ src/modules/auth/cookie.service.ts
@@ -0,0 +1,44 @@
+import { AccessToken, RefreshToken } from '@domain/auth/types';
+import { Injectable } from '@nestjs/common';
+import { AppConfig } from '@technical/app-config/app-config';
+import { type Response } from 'express';
+
+export interface ConfigureOptions {
+  res: Response;
+  accessToken: AccessToken;
+  refreshToken: RefreshToken;
+}
+
+@Injectable()
+export class CookieService {
+  private readonly accessTokenMaxAge: number;
+  private readonly refreshTokenMaxAge: number;
+
+  constructor(private readonly appConfig: AppConfig) {
+    this.accessTokenMaxAge =
+      appConfig.values.JWT_ACCESS_TOKEN_EXPIRATION_SEC * 1000;
+    this.refreshTokenMaxAge =
+      appConfig.values.JWT_REFRESH_TOKEN_EXPIRATION_SEC * 1000;
+  }
+
+  configure({ res, accessToken, refreshToken }: ConfigureOptions): void {
+    const httpOnly = true;
+    const secure = this.appConfig.isProduction();
+    const sameSite = secure ? 'strict' : 'lax';
+
+    res.cookie('access_token', accessToken, {
+      httpOnly,
+      secure,
+      sameSite,
+      maxAge: this.accessTokenMaxAge,
+    });
+
+    res.cookie('refresh_token', refreshToken, {
+      httpOnly,
+      secure,
+      sameSite,
+      path: '/api/auth/refresh',
+      maxAge: this.refreshTokenMaxAge,
+    });
+  }
+}
