6b69f88a5ff6241c4b5197e23f7eca8256169d80
Bartosz Śnieżek <bartosz.sniezek@outlook.com>
2026-03-02 23:19:40 +0100

    Add blacklist and isBlacklisted methods to TokenService

--- src/domain/auth/services/token.service.ts
+++ src/domain/auth/services/token.service.ts
@@ -6,14 +6,39 @@ import { DatetimeService } from '@technical/datetime/datetime.service';
 import { AccessToken, AccessTokenPayload, RefreshToken } from '../types';
 import { RefreshTokenEntity } from '@domain/auth/entities/refresh-token.entity';
 import { randomBytes, randomUUID } from 'crypto';
+import { UserId } from '@modules/users/types';
+import { AppCacheService } from '@technical/cache/app-cache.service';
 
 @Injectable()
 export class TokenService {
+  private readonly blacklistTTL: number;
   constructor(
     private readonly appConfig: AppConfig,
     private readonly jwtService: JwtService,
     private readonly datetimeService: DatetimeService,
-  ) {}
+    private readonly cacheService: AppCacheService,
+  ) {
+    this.blacklistTTL =
+      appConfig.values.JWT_REFRESH_TOKEN_EXPIRATION_SEC * 1000;
+  }
+
+  private composeBlacklistKey(userId: UserId, sessionId: string): string {
+    return `blacklist:${userId}:${sessionId}`;
+  }
+
+  async blacklist(userId: UserId, sessionId: string): Promise<void> {
+    await this.cacheService.set(
+      this.composeBlacklistKey(userId, sessionId),
+      '1',
+      this.blacklistTTL,
+    );
+  }
+
+  async isBlacklisted(userId: UserId, sessionId: string): Promise<boolean> {
+    return this.cacheService
+      .get(this.composeBlacklistKey(userId, sessionId))
+      .then((v) => v != null);
+  }
 
   async generateAccessToken(
     user: UserEntity,
