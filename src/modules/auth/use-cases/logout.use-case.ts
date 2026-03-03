f73936777c5c07b91203fec63841335e5e56a231
Bartosz Śnieżek <bartosz.sniezek@outlook.com>
2026-03-03 20:29:03 +0100

    Add session blacklist check in LogoutUseCase, add unit tests

--- src/modules/auth/use-cases/logout.use-case.ts
+++ src/modules/auth/use-cases/logout.use-case.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { RefreshTokenEntity } from '../../../domain/auth/entities/refresh-token.entity';
+import { IsNull, Repository } from 'typeorm';
+import { RefreshTokenEntity } from '@domain/auth/entities/refresh-token.entity';
 import { AuthenticatedUser } from '@domain/auth/types';
 import { DatetimeService } from '@technical/datetime/datetime.service';
+import { TokenService } from '@domain/auth/services/token.service';
 
 @Injectable()
 export class LogoutUseCase {
@@ -11,18 +12,29 @@ export class LogoutUseCase {
     @InjectRepository(RefreshTokenEntity)
     private readonly refreshTokenRepository: Repository<RefreshTokenEntity>,
     private readonly datetimeService: DatetimeService,
+    private readonly tokenService: TokenService,
   ) {}
 
   async execute(user: AuthenticatedUser): Promise<void> {
+    const isSessionBlacklisted = await this.tokenService.isSessionBlacklisted(
+      user.userId,
+      user.sessionId,
+    );
+
+    if (isSessionBlacklisted) return;
+
     await this.refreshTokenRepository.update(
       {
         userId: user.userId,
         family: user.sessionId,
+        revokedAt: IsNull(),
       },
       {
         revokedAt: this.datetimeService.new(),
       },
     );
+
+    await this.tokenService.blacklistSession(user.userId, user.sessionId);
 
     return;
   }
