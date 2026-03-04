66ecd47f7be77b56345d9e20b1362ab2cac1cfec
Bartosz Śnieżek <bartosz.sniezek@outlook.com>
2026-03-04 13:06:48 +0100

    use PostgreSqlContainer for integration and e2e tests

--- test/setup/setup.ts
+++ test/setup/setup.ts
@@ -1,7 +1,30 @@
 import { execSync } from 'child_process';
+import { PostgreSqlContainer } from '@testcontainers/postgresql';
+import { randomBytes } from 'crypto';
 
-export function setup() {
-  execSync('NODE_ENV=test tsx db/reset-database.ts', {
+export default async function setup() {
+  const database = `${Date.now()}-${randomBytes(32).toString('hex')}`;
+  const pgContainer = await new PostgreSqlContainer(`postgres:18.1`)
+    .withDatabase(database)
+    .withCommand([
+      'postgres',
+      '-c',
+      'wal_level=logical',
+      '-c',
+      'max_wal_senders=4',
+      '-c',
+      'max_replication_slots=4',
+    ])
+    .start();
+
+  process.env.DATABASE_URL = pgContainer.getConnectionUri();
+
+  execSync('tsx db/reset-database.ts', {
     stdio: 'inherit',
   });
+
+  return function teardown() {
+    console.log('Vitest setup teardown');
+    pgContainer.stop().then(() => console.log('PostgreSqlContainer stopped'));
+  };
 }
