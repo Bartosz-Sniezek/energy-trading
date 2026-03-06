129cf604bf5147daec73f8a3ea1b70cbad1eddd7
Bartosz Śnieżek <bartosz.sniezek@outlook.com>
2026-03-06 19:32:34 +0100

    Handle errors in UsersOutboxEmailConsumer

--- src/modules/kafka/kafka.module.ts
+++ src/modules/kafka/kafka.module.ts
@@ -3,7 +3,6 @@ import { KafkaJS } from '@confluentinc/kafka-javascript';
 import { createTopics, CreateTopticOptions } from './create-topics';
 import { AppConfig } from '@technical/app-config/app-config';
 import { AppConfigModule } from '@technical/app-config/app-config.module';
-import { ConsumerFactory } from './consumer.factory';
 import { KAFKA_SERVICE } from './constants';
 
 export interface KafkaModuleOptions {
@@ -33,9 +32,8 @@ export class KafkaModule {
             return kafka;
           },
         },
-        ConsumerFactory,
       ],
-      exports: [KAFKA_SERVICE, ConsumerFactory],
+      exports: [KAFKA_SERVICE],
     };
   }
 }
