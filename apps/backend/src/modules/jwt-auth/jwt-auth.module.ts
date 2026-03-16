6615f013c5f6331dc176e9dec15cb6d420543965
Bartosz Śnieżek <bartosz.sniezek@outlook.com>
2026-03-16 13:19:06 +0100

    Fix JwtAuthModule

--- apps/backend/src/modules/jwt-auth/jwt-auth.module.ts
+++ apps/backend/src/modules/jwt-auth/jwt-auth.module.ts
@@ -1,9 +1,13 @@
+import { TokenService } from '@domain/auth/services/token.service';
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { AppConfigModule } from '@technical/app-config/app-config.module';
+import { AppCacheModule } from '@technical/cache/app-cache.module';
+import { DatetimeService } from '@technical/datetime/datetime.service';
 
 @Module({
-  imports: [AppConfigModule, JwtModule],
-  exports: [AppConfigModule, JwtModule],
+  imports: [AppConfigModule, AppCacheModule, JwtModule],
+  providers: [TokenService, DatetimeService],
+  exports: [AppConfigModule, JwtModule, AppCacheModule, TokenService, DatetimeService],
 })
 export class JwtAuthModule {}
